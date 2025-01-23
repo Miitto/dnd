22c8ad129b6d9d6fed75e2929244cff46c793216
= <=>
2025-01-23 19:56:36 +0000

    Disable menubar

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,23 @@ fn main() {
     dioxus::logger::initialize_default();
     tracing::info!("============================= Starting Dioxus =============================");
 
-    dioxus::launch(ui::App);
+    #[cfg(feature = "desktop")]
+    fn launch_app() {
+        use dioxus::desktop::WindowBuilder;
+
+        dioxus::LaunchBuilder::new()
+            .with_cfg(
+                dioxus::desktop::Config::default()
+                    .with_menu(None)
+                    .with_window(WindowBuilder::new().with_title("DnD 5e")),
+            )
+            .launch(ui::App);
+    }
+
+    #[cfg(not(feature = "desktop"))]
+    fn launch_app() {
+        dioxus::launch(ui::App);
+    }
+
+    launch_app();
 }
